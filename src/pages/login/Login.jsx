fe0664fbb19d4a9ef5dd70e2e3d79d03fc44f522
Ângelo Hank <hankangelo6@gmail.com>
2024-06-24 21:10:07 -0300

    feat: estilizando tela de login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
-import './Style.css';
+import { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("Dados de Login:", { username, password });
+  };
+
   return (
-    <div className="main-login">
-      <div className="right-login">
-        <div className="card-login">
-          <h2>Login</h2>
-          <div className="textfield">
-            <label htmlFor="usuario">Usuário</label>
-            <input type="text" name="usuario" placeholder="Usuário" />
-          </div>
-          <div className="textfield">
-            <label htmlFor="senha">Senha</label>
-            <input type="password" name="senha" placeholder="Senha" />
-          </div>
-          <button className="btn-login">Login</button>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Acesse o sistema</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="E-mail"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
         </div>
-      </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu sua senha?</a>
+        </div>
+        <button type="submit">Login</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registar</a>{" "}
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
